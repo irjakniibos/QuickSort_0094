66b0ecdaa3e0ba36a1314957f515247eb229890d
irjakniibos <irzayaumilsyahrar55@gmail.com>
2024-03-29 18:54:23 +0700

    membuat perkondisian if

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -89,5 +89,8 @@ void display(){
     cout << "Sorted Array" << endl ;
     cout << "-----------------------" << endl ;
 
-    
+    for(int i = 0; i < n;i++){
+        cout << arr[i] << " ";
+    }
+
 }
